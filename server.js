d61de2a27aab939d91e549ed72774c0b0c57422b
fozilovsherzod11-rgb <fozilovsherzod11@gmail.com>
2026-02-07 18:52:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,25 +13,23 @@ app.use(express.static(__dirname));
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    // 1. Синхрон видео
+    // 1. Синхрон
     socket.on('sync_action', (data) => {
         socket.broadcast.emit('sync_action', data);
     });
 
-    // 2. Эмоция
+    // 2. Реакция
     socket.on('send_reaction', () => {
         io.emit('show_reaction');
     });
 
     // 3. Смена видео
     socket.on('change_video', (videoId) => {
-        console.log('Changing video to:', videoId);
         io.emit('update_video', videoId);
     });
 
     // 4. Чат
     socket.on('chat_message', (msg) => {
-        // Отправляем сообщение всем, включая отправителя
         io.emit('chat_message', msg);
     });
 });
